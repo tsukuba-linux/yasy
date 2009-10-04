8ed887f2bdb8fe91df387fa2710d858501048e97
KURAZEKO Kyohe <tnzk@patricia.(none)>
2009-10-04 18:42:08 +0900

    Convert implemented

--- yasy.c
+++ yasy.c
@@ -19,10 +19,6 @@
 
 #define DEBUG
 
-AVFrame *picture, *tmp_picture;
-uint8_t *video_outbuf;
-int frame_count, video_outbuf_size;
-
 float t, tincr, tincr2;
 int16_t *samples;
 uint8_t *audio_outbuf;
@@ -47,9 +43,9 @@ void yasy_overlay( AVFrame* frame, gdImage* img,
       int c_index = gdImageGetPixel( img, x, y);
       double a = (double)( 127 - gdImageAlpha( img, c_index)) / 127.0;
 
-      r = frame->data[0][p] * (1.0 - a) + (double)gdImageRed( img, c_index) * a;
+      r = frame->data[0][p  ] * (1.0 - a) + (double)gdImageRed(   img, c_index) * a;
       g = frame->data[0][p+1] * (1.0 - a) + (double)gdImageGreen( img, c_index) * a;
-      b = frame->data[0][p+2] * (1.0 - a) + (double)gdImageBlue( img, c_index) * a;
+      b = frame->data[0][p+2] * (1.0 - a) + (double)gdImageBlue(  img, c_index) * a;
 						  
       frame->data[0][p] = r;
       frame->data[0][p+1] = g;
@@ -78,124 +74,6 @@ void SaveFrame(AVFrame *pFrame, int width, int height, int iFrame)
   fclose(pFile);
 }
 
-static void get_audio_frame(int16_t *samples, int frame_size, int nb_channels)
-{
-    int j, i, v;
-    int16_t *q;
-
-    q = samples;
-    for(j=0;j<frame_size;j++) {
-        v = (int)(sin(t) * 10000);
-        for(i = 0; i < nb_channels; i++)
-            *q++ = v;
-        t += tincr;
-        tincr += tincr2;
-    }
-}
-
-static void write_audio_frame(AVFormatContext *oc, AVStream *st)
-{
-    AVCodecContext *c;
-    AVPacket pkt;
-    av_init_packet(&pkt);
-
-    c = st->codec;
-
-    get_audio_frame(samples, audio_input_frame_size, c->channels);
-
-    pkt.size= avcodec_encode_audio(c, audio_outbuf, audio_outbuf_size, samples);
-
-    if (c->coded_frame->pts != AV_NOPTS_VALUE)
-        pkt.pts= av_rescale_q(c->coded_frame->pts, c->time_base, st->time_base);
-    pkt.flags |= PKT_FLAG_KEY;
-    pkt.stream_index= st->index;
-    pkt.data= audio_outbuf;
-
-    /* write the compressed frame in the media file */
-    if (av_interleaved_write_frame(oc, &pkt) != 0) {
-        fprintf(stderr, "Error while writing audio frame\n");
-        exit(1);
-    }
-}
-
-void open_audio(AVFormatContext *oc, AVStream *st)
-{
-    AVCodecContext *c;
-    AVCodec *codec;
-
-    c = st->codec;
-
-    /* find the audio encoder */
-    codec = avcodec_find_encoder(c->codec_id);
-    if (!codec) {
-        fprintf(stderr, "codec not found\n");
-        exit(1);
-    }
-
-    /* open it */
-    if (avcodec_open(c, codec) < 0) {
-        fprintf(stderr, "could not open codec\n");
-        exit(1);
-    }
-
-    /* init signal generator */
-    t = 0;
-    tincr = 2 * M_PI * 110.0 / c->sample_rate;
-    /* increment frequency by 110 Hz per second */
-    tincr2 = 2 * M_PI * 110.0 / c->sample_rate / c->sample_rate;
-
-    audio_outbuf_size = 10000;
-    audio_outbuf = av_malloc(audio_outbuf_size);
-
-    /* ugly hack for PCM codecs (will be removed ASAP with new PCM
-       support to compute the input frame size in samples */
-    if (c->frame_size <= 1) {
-        audio_input_frame_size = audio_outbuf_size / c->channels;
-        switch(st->codec->codec_id) {
-        case CODEC_ID_PCM_S16LE:
-        case CODEC_ID_PCM_S16BE:
-        case CODEC_ID_PCM_U16LE:
-        case CODEC_ID_PCM_U16BE:
-            audio_input_frame_size >>= 1;
-            break;
-        default:
-            break;
-        }
-    } else {
-        audio_input_frame_size = c->frame_size;
-    }
-    samples = av_malloc(audio_input_frame_size * 2 * c->channels);
-}
-
-
-AVStream *add_audio_stream(AVFormatContext *oc, enum CodecID codec_id)
-{
-
-    AVCodecContext *c;
-    AVStream *st;
-
-    st = av_new_stream(oc, 1);
-
-    c = st->codec;
-    c->codec_id = codec_id;
-    c->codec_type = CODEC_TYPE_AUDIO;
-
-    /* put sample parameters */
-    c->bit_rate = 64000;
-    c->sample_rate = 44100;
-    c->channels = 2;
-    return st;
-}
-
-static void close_audio(AVFormatContext *oc, AVStream *st)
-{
-    avcodec_close(st->codec);
-
-    av_free(samples);
-    av_free(audio_outbuf);
-}
-
-
 int main(int argc, char **argv)
 {
   
@@ -207,7 +85,9 @@ int main(int argc, char **argv)
   AVOutputFormat  *out_fmt;
   AVFormatContext *out_fmt_ctx;
   AVCodecContext *out_cdc_ctx;
+  AVCodecContext *out_cdc_ctx_audio;
   AVCodec *out_codec;
+  AVCodec *out_codec_audio;
   AVStream *out_stream_audio;
   AVStream *out_stream_video;
   double out_audio_pts;
@@ -217,7 +97,9 @@ int main(int argc, char **argv)
   AVInputFormat  *in_fmt;
   AVFormatContext *in_fmt_ctx;
   AVCodecContext *in_cdc_ctx;
+  AVCodecContext *in_cdc_ctx_audio;
   AVCodec *in_cdc;
+  AVCodec *in_cdc_audio;
   int in_stream_audio;
   int in_stream_video;
   double in_audio_pts;
@@ -318,8 +200,13 @@ int main(int argc, char **argv)
     }
   }
 
+  // Get codec to decode video of input 
   in_cdc_ctx = in_fmt_ctx->streams[in_stream_video]->codec;
   in_cdc = avcodec_find_decoder( in_cdc_ctx->codec_id);
+
+  // Get codec to decode audio of input 
+  in_cdc_ctx_audio = in_fmt_ctx->streams[in_stream_audio]->codec;
+  in_cdc_audio = avcodec_find_decoder( in_cdc_ctx_audio->codec_id);
 
   bitrate = in_cdc_ctx->bit_rate;
   width  = in_cdc_ctx->width;
@@ -335,16 +222,18 @@ int main(int argc, char **argv)
   printf( "Frame-rate: %i/%i\n", framerate.den, framerate.num);
 #endif
 
+  // Get codecs for input video and audio
   avcodec_open( in_cdc_ctx, in_cdc);
+  avcodec_open( in_cdc_ctx_audio, in_cdc_audio);
 
-  // add out stream
+  enum CodecID codec_id;
+  // add out video stream and configure
   out_stream_video = av_new_stream( out_fmt_ctx, 0);
-  enum CodecID codec_id = av_guess_codec( out_fmt_ctx->oformat,
-					  NULL,
-					  out_fmt_ctx->filename,
-					  NULL, 
-					  CODEC_TYPE_VIDEO);
-
+  codec_id = av_guess_codec( out_fmt_ctx->oformat,
+			     NULL,
+			     out_fmt_ctx->filename,
+			     NULL, 
+			     CODEC_TYPE_VIDEO);
   out_codec = avcodec_find_encoder(codec_id);
   if(out_codec == NULL) error("can't find codec(encoder).");
   out_cdc_ctx = out_stream_video->codec;
@@ -354,7 +243,6 @@ int main(int argc, char **argv)
   out_cdc_ctx->height = height;
   out_cdc_ctx->time_base.num = framerate.num;
   out_cdc_ctx->time_base.den = framerate.den;
-
   out_cdc_ctx->pix_fmt = PIX_FMT_NONE;
   if( out_codec && out_codec->pix_fmts){
     const enum PixelFormat *p = out_codec->pix_fmts;
@@ -365,26 +253,40 @@ int main(int argc, char **argv)
   }
   if( out_fmt->flags & AVFMT_GLOBALHEADER)
     out_cdc_ctx->flags |= CODEC_FLAG_GLOBAL_HEADER;
-
-
-  if ( out_fmt->audio_codec != CODEC_ID_NONE) {
-    out_stream_audio = add_audio_stream( out_fmt_ctx, out_fmt->audio_codec);
-  }
+  /*
+  // add out auido stream and configure
+  out_stream_audio = av_new_stream( out_fmt_ctx, 0);
+  
+  codec_id = av_guess_codec( out_fmt_ctx->oformat,
+			     NULL,
+			     out_fmt_ctx->filename,
+			     NULL, 
+			     CODEC_TYPE_AUDIO);
+  out_codec_audio = avcodec_find_encoder(codec_id);
+  if(out_codec_audio == NULL) error("can't find codec(encoder).");
+  out_cdc_ctx_audio = out_stream_audio->codec;
+  out_cdc_ctx_audio->codec_id = out_codec_audio->id;
+  out_cdc_ctx_audio->codec_type = CODEC_TYPE_AUDIO;
+  out_cdc_ctx_audio->bit_rate    = in_cdc_ctx_audio->bit_rate;
+  out_cdc_ctx_audio->sample_rate = in_cdc_ctx_audio->sample_rate;
+  out_cdc_ctx_audio->channels = 2;
+  out_cdc_ctx_audio->time_base.num = in_fmt_ctx->streams[in_stream_audio]->time_base.num;
+  out_cdc_ctx_audio->time_base.den = in_fmt_ctx->streams[in_stream_audio]->time_base.den;
+  if( out_fmt->flags & AVFMT_GLOBALHEADER)
+    out_cdc_ctx->flags |= CODEC_FLAG_GLOBAL_HEADER;
+  */
   av_set_parameters( out_fmt_ctx, NULL);
-
+  
   dump_format( out_fmt_ctx, 0, o_filename, 1);
-
-
-  int ret = avcodec_open( out_cdc_ctx, out_codec);
-  if(ret != 0) error("can't open codec(encoder).");
-
+  
+  int ret;
+  ret = avcodec_open( out_cdc_ctx, out_codec);
+  //ret = avcodec_open( out_cdc_ctx_audio, out_codec_audio);
+  
   if (! ( out_fmt->flags & AVFMT_NOFILE )) {
     ret = url_fopen(&out_fmt_ctx->pb, out_fmt_ctx->filename, URL_WRONLY);
     if (ret < 0) error("can't open output file.");
   }
-  
-
-  if(out_stream_audio) open_audio( out_fmt_ctx, out_stream_audio);
 
   av_write_header( out_fmt_ctx);
 
@@ -392,19 +294,69 @@ int main(int argc, char **argv)
   frame_rgb = avcodec_alloc_frame();
 
   int buf_size = out_cdc_ctx->width * out_cdc_ctx->height * 4;
-  uint8_t *buf = av_malloc(buf_size);
-  
+  uint8_t* buf = av_malloc(buf_size);
+  int a_buf_size = AVCODEC_MAX_AUDIO_FRAME_SIZE * 2;
+  uint8_t* a_buf = av_malloc(a_buf_size);
 
   out_cdc_ctx->bit_rate = 128000;
   out_cdc_ctx->qcompress = 0.0;
   out_cdc_ctx->qmin = 0;
   out_cdc_ctx->qmax = 2;
 
+  /*
+  out_cdc_ctx_audio->bit_rate = in_cdc_ctx_audio->bit_rate;
+  out_cdc_ctx_audio->sample_rate = in_cdc_ctx_audio->sample_rate;
+  out_cdc_ctx_audio->channels    = in_cdc_ctx_audio->channels;
+  out_cdc_ctx_audio->sample_fmt  = in_cdc_ctx_audio->sample_fmt;
+  */
+
   i = 0;
+  int x;
+  int y;
+  int audio_size = FF_INPUT_BUFFER_PADDING_SIZE + AVCODEC_MAX_AUDIO_FRAME_SIZE;
+  int16_t* sample = (int16_t*)malloc( audio_size);
+
   while( av_read_frame( in_fmt_ctx, &packet) >= 0){
-    int x;
-    int y;
-    // Check if this packet is from video stream
+    
+    /*
+    // the packet is from the audio stream
+    if( packet.stream_index == in_stream_audio){
+      avcodec_decode_audio2( in_cdc_ctx_audio,
+			     sample,
+			     &audio_size,
+			     packet.data,
+			     packet.size);
+      if( audio_size){
+	int out_size = avcodec_encode_audio( out_cdc_ctx_audio, a_buf, a_buf_size, sample);
+	printf( "%i\n", out_fmt_ctx->nb_streams);
+	if (out_size == 0){
+	  continue;
+	} else if (out_size < 0){
+	  error("can't encode frame.");
+	}
+
+	AVPacket packet;
+	av_init_packet(&packet);
+
+	packet.stream_index = out_stream_audio->index;
+	packet.data = a_buf;
+	packet.size = a_buf_size;
+	packet.pts = av_rescale_q( out_cdc_ctx_audio->coded_frame->pts, 
+				   out_cdc_ctx_audio->time_base,
+				   out_stream_audio->time_base);
+
+	if( out_cdc_ctx->coded_frame->key_frame)
+	  packet.flags |= PKT_FLAG_KEY;
+
+	int ret = av_interleaved_write_frame( out_fmt_ctx, &packet);
+	if(ret != 0) error("can't write frame.");
+	
+	av_free_packet( &packet);
+      }
+    }
+    */
+
+    // the packet is from the video stream
     if( packet.stream_index == in_stream_video){
 
       avcodec_decode_video( in_cdc_ctx,
@@ -503,9 +455,7 @@ int main(int argc, char **argv)
 	av_free_packet( &packet);
 	printf( "%i\n", i);
 	i++;
-#ifdef DEBUG
-	if( i > 30) break;
-#endif
+	//	if( i >= 300)break;
       }
     }
     av_free_packet( &packet);
@@ -514,8 +464,6 @@ int main(int argc, char **argv)
   puts("Encode completed");
   av_write_trailer( out_fmt_ctx);
   puts("Trailer writing completed");  
-
-  if (out_stream_audio) close_audio( out_fmt_ctx, out_stream_audio);
 
   for(i = 0; i < out_fmt_ctx->nb_streams; i++) {
     av_freep(&out_fmt_ctx->streams[i]->codec);
@@ -531,9 +479,11 @@ int main(int argc, char **argv)
   av_free( frame);
   av_free( frame_rgb);
   avcodec_close( in_cdc_ctx);
+  avcodec_close( in_cdc_ctx_audio);
   av_close_input_file( in_fmt_ctx);
   av_free( out_fmt_ctx);
   gdImageDestroy( img);
+  free( sample);
   puts("Allocated memories released");
   puts("ALL YOUR BASES ARE BELONG TO US");
 
